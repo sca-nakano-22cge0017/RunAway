be994dafb86692fef1fd664e6ee60c4a4b20beef
NakanoAyame <sdt22cge0017@edu.sca.ac.jp>
2023-03-05 18:39:22 +0900

    ゲーム終了演出作成

--- Assets/Nakano/Scripts/ExitStaging.cs
+++ Assets/Nakano/Scripts/ExitStaging.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExitStaging : MonoBehaviour
 {
-    [SerializeField] private GameObject Circle;
-
     private GameObject ExitButton;
     ExitButtonController exitButtonController;
 
-    Vector2 staging = new Vector2(0.1f, 0.1f);
+    [SerializeField] private GameObject Line;
+    [SerializeField] private Image Circle1;
+    [SerializeField] private Image Circle2;
+
+    Vector2 stagingScale = new Vector2(0.1f, 0.1f);
+    [SerializeField] private float _growingSpeed;
+
+    [SerializeField] private float _rotateSpeed;
 
     void Start()
     {
-        Circle.SetActive(false);
-        staging = new Vector2(0.1f, 0.1f);
+        Line.SetActive(false);
+        stagingScale = new Vector2(0.1f, 0.1f);
+
+        Circle1.fillAmount = 0;
+        Circle2.fillAmount = 0;
     }
 
     void Update()
@@ -22,12 +31,18 @@ public class ExitStaging : MonoBehaviour
         ExitButton = GameObject.Find("Exit");
         exitButtonController = ExitButton.GetComponent<ExitButtonController>();
 
-        if (exitButtonController.startStaging == true)
+        if (exitButtonController.startStaging == false)
         {
-            Circle.SetActive(true);
+            Line.SetActive(true);
 
-            staging.x += 0.1f;
-            Circle.transform.localScale = staging;
+            stagingScale.x += _growingSpeed * Time.deltaTime;
+            Line.transform.localScale = stagingScale;
+        }
+
+        if (stagingScale.x >= 40.0f)
+        {
+            Circle1.fillAmount += _rotateSpeed * Time.deltaTime;
+            Circle2.fillAmount += _rotateSpeed * Time.deltaTime;
         }
     }
 }
