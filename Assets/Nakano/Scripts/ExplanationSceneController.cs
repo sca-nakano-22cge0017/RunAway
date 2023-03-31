7e95ff0b39b2296dd13ab5e05172dfb988d56b0e
NakanoAyame <sdt22cge0017@edu.sca.ac.jp>
2023-03-31 17:46:33 +0900

    操作説明画面作成

--- Assets/Nakano/Scripts/ExplanationSceneController.cs
+++ Assets/Nakano/Scripts/ExplanationSceneController.cs
@@ -5,16 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class ExplanationSceneController : MonoBehaviour
 {
+    [SerializeField] private GameObject Explanation;
+    [SerializeField] private GameObject ItemsExplanation;
+
+    int enter = 0;
+
     void Start()
     {
-        
+        Explanation.SetActive(true);
+        ItemsExplanation.SetActive(false);
     }
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Return))
         {
-            SceneManager.LoadScene("MainGameScene");
+            enter += 1;
+        }
+
+        switch(enter)
+        {
+            case 1:
+                ItemsExplanation.SetActive(true);
+            break;
+            case 2:
+                SceneManager.LoadScene("MainGameScene");
+            break;
         }
     }
 }
