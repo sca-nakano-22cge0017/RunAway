9113476a00f8821eed44cae3c5abb1f9683a2979
NakanoAyame <sdt22cge0017@edu.sca.ac.jp>
2023-04-05 08:57:20 +0900

    ファンの移動作成(途中)

--- Assets/Nakano/Scripts/FanMove.cs
+++ Assets/Nakano/Scripts/FanMove.cs
@@ -1,27 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FanMove : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private GameObject player;
+    [SerializeField] private float fanRange;
+    [SerializeField] private Text textSample;
+    GameObject Player;
 
     void Start()
     {
-        
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
-        Vector3 player = GameObject.Find("Player").transform.position;
+        /*Vector3 player = GameObject.Find("Player").transform.position;
         Transform myTransform = this.transform;
         Vector3 myPos = myTransform.position;
         
-        if (myPos.x - player.x >= 1.5f)
+        if (myPos.x - player.x <= fanRange / 100 && myPos.x - player.x >= 0)
         {
-            Debug.Log("èPÇ§");
+            Debug.Log(myPos.x - player.x);
+            textSample.text = "èPÇ§";
         }
+        else { textSample.text = "";}*/
 
         transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
     
@@ -29,5 +34,18 @@ public class FanMove : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            textSample.text = "èPÇ§";
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        textSample.text = "";
     }
 }
