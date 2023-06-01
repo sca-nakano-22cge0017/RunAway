e35690738f505b4cf67f4d65c702a891d52f5608
sdt22cge0002@edu.sca.ac.jp <oikawatakumi0212@gmail.com>
2023-06-01 23:19:59 +0900

    animation関連のエラーチェック

---
+++ Assets/Nakano/Scripts/FanSpawnManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FanSpawn
+{
+    public class FanSpawnManager : MonoBehaviour
+    {
+        public GameObject[] fanPrefab;
+        int number;
+        [SerializeField] private float distanceMin;
+        [SerializeField] private float distanceMax;
+        private float distance;
+        private float time;
+        [SerializeField] private float timeInterval;
+
+        void Start()
+        {
+            time = timeInterval;
+        }
+
+        void Update()
+        {
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                distance = Random.Range(distanceMin, distanceMax);
+                number = Random.Range(0, fanPrefab.Length);
+                Instantiate(fanPrefab[number], new Vector3(distance, -2f, 0), transform.rotation);
+                time = timeInterval;
+            }
+        }
+    }
+
+}
