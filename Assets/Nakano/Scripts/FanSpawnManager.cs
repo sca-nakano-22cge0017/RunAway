8b1e6cabd8eb592a0f963bc0a547c144cfe117a6
ShunSato <sdt22cge0013@edu.sca.ac.jp>
2023-06-01 23:46:09 +0900

    エラーの修正とスクリプトの整理

--- Assets/Nakano/Scripts/FanSpawnManager.cs
+++ Assets/Nakano/Scripts/FanSpawnManager.cs
@@ -2,34 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace FanSpawn
+public class FanSpawnManager : MonoBehaviour
 {
-    public class FanSpawnManager : MonoBehaviour
+    public GameObject[] fanPrefabA;
+    public GameObject[] fanPrefabB;
+    int number;
+    [SerializeField] private float distanceMin;
+    [SerializeField] private float distanceMax;
+    private float distance;
+    private float time;
+    [SerializeField] private float timeInterval;
+
+    int selectCharacter = SelectSceneManager.selectCharacter;
+
+    void Start()
     {
-        public GameObject[] fanPrefab;
-        int number;
-        [SerializeField] private float distanceMin;
-        [SerializeField] private float distanceMax;
-        private float distance;
-        private float time;
-        [SerializeField] private float timeInterval;
+        time = timeInterval;
+        selectCharacter = 1; //Debug
+    }
 
-        void Start()
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if(time <= 0)
         {
-            time = timeInterval;
-        }
-
-        void Update()
-        {
-            time -= Time.deltaTime;
-            if (time <= 0)
+            if(selectCharacter == 1)
             {
                 distance = Random.Range(distanceMin, distanceMax);
-                number = Random.Range(0, fanPrefab.Length);
-                Instantiate(fanPrefab[number], new Vector3(distance, -2f, 0), transform.rotation);
+                number = Random.Range(0, fanPrefabA.Length);
+                Instantiate(fanPrefabA[number], new Vector3(distance, -2.3f, 0), transform.rotation);
+                time = timeInterval;
+            }
+            if(selectCharacter == 2)
+            {
+                distance = Random.Range(distanceMin, distanceMax);
+                number = Random.Range(0, fanPrefabB.Length);
+                Instantiate(fanPrefabB[number], new Vector3(distance, -2.3f, 0), transform.rotation);
                 time = timeInterval;
             }
         }
     }
-
 }
